c10e627719acb844817cd6b3776e60145833a4a4
Kai Zhang <kaizen.agility@gmail.com>
2022-11-27 23:09:59 -0500

    Pass post object from Feed.js to Story.js without destructuring object

--- ogfb/src/Feed.js
+++ ogfb/src/Feed.js
@@ -1,13 +1,18 @@
 import Story from './Story'
 
 export default function Feed () {
+
+    const post = {
+        'name': 'Mark Zuckerburg',
+        'status': 'is dreaming of building a Metaverse',
+        'time': 'November 27, 2006, 3:00pm',
+        'likes': 0,
+    }
+
     return(
       <div className="feed">
         <h3>Feed</h3>
-        <Story />
-        <Story />
-        <Story />
-        <Story />
+        <Story post={post} />
       </div>
     )    
 }
--- ogfb/src/Story.js
+++ ogfb/src/Story.js
@@ -1,7 +1,15 @@
-export default function Story () {
+const Story = (props) => {
+
+    const post = props.post;
+
   return(
     <>
-        <p>Story</p>
+        <p>{post["name"]}</p>
+        <p>{post["status"]}</p>
+        <p>{post["time"]}</p>
+        <p>{post["likes"]}</p>
     </>
   )
 }
+
+export default Story;
