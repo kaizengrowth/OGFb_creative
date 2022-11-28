0422acfb8023bbbaecbc6ea26fb386fb9432961b
Kai Zhang <kaizen.agility@gmail.com>
2022-11-27 21:59:08 -0500

    Create MenuItem.js with simple dummy text and display it inside of Nav.js

---
+++ ogfb/src/MenuItem.js
@@ -0,0 +1,7 @@
+export default function MenuItem () {
+  return (
+    <>
+        <h5>Item 1</h5>
+    </>
+  )
+}
--- ogfb/src/Nav.js
+++ ogfb/src/Nav.js
@@ -1,6 +1,12 @@
+import logo from './old_fb_banner.png';
+import './App.css';
+import MenuItem from './MenuItem'
+
 export default function Nav () {
   return( 
     <>
+        <img src={logo} className="App-logo" alt="logo" />
+        <MenuItem />
     </>
   )
 }
