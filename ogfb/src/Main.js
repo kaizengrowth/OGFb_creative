f22708885f500be2c911ec84b143a5ce21a4b6e0
Kai Zhang <kaizen.agility@gmail.com>
2022-11-27 21:42:19 -0500

    Create Main.js and Nav.js, and move text from App.js to Main.js"

---
+++ ogfb/src/Main.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function Main() {
+    return( 
+      <>
+        <header className="App-header">  
+            <p>
+            Hello, the Book of Face: 🎉!
+            </p>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            I'm learning React.
+            </a>
+        </header>
+      </>
+    )
+  }
