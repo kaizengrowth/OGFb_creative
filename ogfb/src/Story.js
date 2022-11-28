6ef708bd47456cda900eeee1155ac5c85c2822f4
Kai Zhang <kaizen.agility@gmail.com>
2022-11-28 00:15:20 -0500

    Import useState and create [hearts, setHearts] hook for likes

--- ogfb/src/Story.js
+++ ogfb/src/Story.js
@@ -1,15 +1,19 @@
+import {useState} from 'react';
+
 const Story = (props) => {
 
     // const post = props.post;
 
     const {name, status, time, likes} = props.post;
 
+    const [hearts, setHearts] = useState(likes + 3);
+
   return(
     <div className="story">
         <p className="name">{name}</p>
         <p className="time">{time}</p>
         <p className="status">{status}</p>
-        <button className="likes">💙 {likes}</button>
+        <button className="likes">💙 {hearts}</button>
         <hr/>
     </div>
   )
