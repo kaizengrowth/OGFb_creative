35e3e50aae80938a1151eaf5e329f186d71eefe3
Kai Zhang <kaizen.agility@gmail.com>
2022-11-28 17:21:26 -0500

    Fetching from BoredAPI using useEffect() hook

--- ogfb/src/SideNav.js
+++ ogfb/src/SideNav.js
@@ -1,7 +1,27 @@
+import {useState, useEffect} from 'react'
+
 export default function SideNav () {
+
+  const [myStatus, setStatus] = useState("");
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await fetch('https://www.boredapi.com/api/activity');
+      const data = await response.json();
+      return data['activity'];
+    }
+    
+    fetchData()
+    .then(randomStatus => setStatus(randomStatus))
+    .catch(console.error);
+
+    
+  }, [])
+
   return(
     <div className="sideNav">
-        <p>SideNav</p>
+        <h5>My Current Status:</h5>
+        <p>{myStatus}</p>
     </div>
-  )    
+  );
 }
