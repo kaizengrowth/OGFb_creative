6f81a063669ba644f4399537258a7cabc807f8ab
Kai Zhang <kaizen.agility@gmail.com>
2022-11-27 23:13:16 -0500

    Demonstrate object destructuring / prop destructuring

--- ogfb/src/Story.js
+++ ogfb/src/Story.js
@@ -1,13 +1,15 @@
 const Story = (props) => {
 
-    const post = props.post;
+    // const post = props.post;
+
+    const {name, status, time, likes} = props.post;
 
   return(
     <>
-        <p>{post["name"]}</p>
-        <p>{post["status"]}</p>
-        <p>{post["time"]}</p>
-        <p>{post["likes"]}</p>
+        <p>{name}</p>
+        <p>{time}</p>
+        <p>{status}</p>
+        <p>{likes}</p>
     </>
   )
 }
