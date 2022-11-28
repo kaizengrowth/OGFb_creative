48c79a20747695d51bbe7adc698ee9bbdcf4b136
Kai Zhang <kaizen.agility@gmail.com>
2022-11-28 02:27:40 -0500

    Add DateTime formatting on click

--- ogfb/src/Story.js
+++ ogfb/src/Story.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import Button from 'react-bootstrap/Button';
+import { format } from 'date-fns'
 
 const Story = (props) => {
 
@@ -7,11 +8,14 @@ const Story = (props) => {
 
     const [hearts, setHearts] = useState(likes + 3);
     const [liked, setLiked] = useState(false);
+    const [datetime, setDatetime] = useState(time)
 
     const handleClick = () => {
       if (liked === false) {
         setHearts(hearts+1);
         setLiked(true);
+        const date = Date.now();
+        setDatetime(format(date, 'PPPPpppp'));
       } else {
         setHearts(hearts-1);
         setLiked(false);
@@ -21,7 +25,7 @@ const Story = (props) => {
   return(
     <div className="story">
         <p className="name">{name}</p>
-        <p className="time">{time}</p>
+        <p className="time">{datetime}</p>
         <p className="status">{status}</p>
         <Button onClick={handleClick} className="likes">💙 {hearts}</Button>
         <hr/>
